4d56edb3d017716b6617ccc9c82bd51e3180de09
whitok355 <3139139@mail.ru>
2024-03-12 15:05:33 +0300

    hw2 v1.3

---
+++ src/seminars/Main.java
@@ -0,0 +1,8 @@
+package seminars;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "X2", 1990);
+        Motorcycle motorcycle = new Motorcycle("Audi", "ms2", 2000);
+    }
+}
--- src/seminars/second/hw/Main.java
+++
@@ -1,8 +0,0 @@
-package seminars.second.hw;
-
-public class Main {
-    public static void main(String[] args) {
-        Car car = new Car("BMW", "X2", 1990);
-        Motorcycle motorcycle = new Motorcycle("Audi", "ms2", 2000);
-    }
-}
